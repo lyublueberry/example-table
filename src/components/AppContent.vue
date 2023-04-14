c7904f4b8b2450f0414f205ccb64c57410dd6ea6
Lara87 <lara.87@bk.ru>
2023-04-15 01:39:04 +0300

    create table

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -16,10 +16,7 @@
         </template>
       </ui-alert>
 
-      <data-table
-        :rows="data"
-        :columns="columns"
-      />
+      <data-table :rows="data" :columns="columns" />
     </template>
   </main>
 </template>
@@ -52,6 +49,50 @@ export default {
         label: 'Money',
         prop: 'money',
         width: '35%',
+      },
+    ],
+    data: [
+      {
+        id: 1,
+        name: 'Bob Lee',
+        money: 1000,
+        date: '2020-02-24',
+      },
+      {
+        id: 2,
+        name: 'John Seek',
+        money: 1999.99,
+        date: '2020-02-25',
+      },
+      {
+        id: 3,
+        name: 'Harry Smith',
+        money: 3000.5,
+        date: '2020-02-26',
+      },
+      {
+        id: 4,
+        name: 'Alex Morphy',
+        money: 4000,
+        date: '2020-02-27',
+      },
+      {
+        id: 5,
+        name: 'Ben Gold',
+        money: 5000,
+        date: '2020-02-28',
+      },
+      {
+        id: 6,
+        name: 'Tim Black',
+        money: 10000,
+        date: '2020-02-28',
+      },
+      {
+        id: 7,
+        name: 'Jimmy Elephant',
+        money: 1000000,
+        date: '2020-02-30',
       },
     ],
   }),
--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -3,14 +3,19 @@
     <div class="data-table__filter">
       <ui-money v-model="moneyFilter" />
     </div>
-
-    <!-- Your component code here -->
-
+    <div class="table-column">
+      <div class="table-column__cell column-cell" v-for="column in columns" :key="column.id"
+        :style="{ maxWidth: column.width }">
+        <span class="column-cell__name">{{ column.label }}</span>
+        <div class="table-row">
+          <div class="table-row__cell row-cell" v-for="row in rows" :key="row.id">
+            <span>{{ row[column.prop] }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="data-table__paginator">
-      <ui-pagination
-        v-model="page"
-        :pages="pageCount"
-      />
+      <ui-pagination v-model="page" :pages="pageCount" />
     </div>
   </div>
 </template>
@@ -42,5 +47,51 @@ export default {
       return Math.ceil(this.rows.length / this.pageSize);
     },
   },
+  methods: {
+
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.data-table {
+  &__filter {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 24px;
+  }
+
+  .table-column {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    &__cell {
+      width: 100%;
+      padding: 12px 0;
+      border: 1px solid currentColor;
+      text-align: center;
+    }
+
+    &__cell:not(:last-child) {
+      border-right: transparent;
+    }
+
+    .column-cell {
+      &__name {
+        font-weight: 600;
+      }
+    }
+
+  }
+
+  .table-row {
+    .row-cell {
+      padding: 12px 0;
+      border-top: 1px solid currentColor;
+
+    }
+  }
+}
+</style>
