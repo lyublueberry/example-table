2016da378498d6fa95b21a061d0091a22d474f2f
Lara87 <lara.87@bk.ru>
2023-04-16 11:09:59 +0300

    change

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -16,9 +16,8 @@
         </div>
       </div>
     </div>
-
     <div class="mobile-table">
-      <div class="mobile-table__item mobile-item" v-for="row in rows" :key="row.id" >
+      <div class="mobile-table__item mobile-item" v-for="row in rows" :key="row.id">
         <div class="mobile-item__row item-row" v-for="column in columns" :key="column.id">
           <p class="item-row__name">{{ column.label }}</p>
           <p class="item-row__value">{{ row[column.prop] }}</p>
@@ -26,12 +25,15 @@
       </div>
     </div>
     <div class="data-table__paginator">
-      <ui-pagination v-model="page" :pages="pageCount" />
-    </div>
+      <ui-pagination v-model="page" :pages="pageCount" :countRowsOnPage="countRowsOnPage" />
+    </div> <div class="">{{ tttt() }}</div>
   </div>
 </template>
 
 <script>
+
+import getPayments from '@/mocks/getPayments';
+
 export default {
 
   name: 'DataTable',
@@ -55,11 +57,18 @@ export default {
 
   computed: {
     pageCount() {
+      console.log(Math.ceil(this.rows.length / this.pageSize));
       return Math.ceil(this.rows.length / this.pageSize);
+    },
+    countRowsOnPage() {
+      console.log(Math.ceil(this.rows.length / this.pageCount));
+      return Math.ceil(this.rows.length / this.pageCount);
     },
   },
   methods: {
-
+    tttt() {
+      getPayments().then((result) => console.log(result.data[0].date));
+    },
   },
   watch: {
     widthScreen(newValue) {
