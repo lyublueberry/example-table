dc87bdc3bd1ae3d08c02d3ab14701d81928f522b
Lara87 <lara.87@bk.ru>
2023-04-25 17:53:14 +0300

    change

--- src/components/UI/Money.vue
+++ src/components/UI/Money.vue
@@ -1,34 +1,43 @@
 <template>
-  <input type="text" v-model.trim="valueInput" class="ui-money" />
+  <input type="text" v-model.trim="valueInput" @keypress="isNumber($event)" @blur="blur" class="ui-money" />
 </template>
 
 <script>
 export default {
-
   name: 'UiMoney',
-
   props: {
     value: {
       type: [Number, String],
-      require: true,
+      required: true,
     },
   },
   computed: {
     valueInput: {
       get() {
-        return this.value.toLocaleString('ru-RU');
+        return this.value;
       },
       set(value) {
-        let formateValue = value;
-        if (/[а-яё]+|[a-z]+|[A-Z]+|[А-ЯЁ]/g.test(formateValue)) {
-          formateValue.replace('');
-        } else {
-          formateValue = (parseFloat(formateValue)).replace(',', '.');
-        }
-        if (value !== formateValue) {
-          this.$emit('input', formateValue);
-        }
+        const formattedValue = this.formatValue(value);
+        this.$emit('input', formattedValue);
       },
+    },
+  },
+  methods: {
+    blur() {
+      this.valueInput = this.valueInput.toFixed(2);
+    },
+    formatValue(val) {
+      const formatted = val.replace(',', '.');
+      const floatted = parseFloat(formatted);
+      return floatted;
+    },
+    isNumber(evt) {
+      const keysAllowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+      const keyPressed = evt.key;
+
+      if (!keysAllowed.includes(keyPressed)) {
+        evt.preventDefault();
+      }
     },
   },
 };
